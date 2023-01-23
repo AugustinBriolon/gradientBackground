393eeda4d41a42d2cd3245519d73ac6b8f057087
Augustin <augustin.briolon@gmail.com>
2023-01-23 15:01:56 +0100

    :lipstick: change some visuals

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -5,19 +5,22 @@ import cards from './cards.json';
 export default function Cards() {
 
   let card = cards.cards;
+  let cardText = document.querySelectorAll('.card');
 
-  document.querySelectorAll('.cards').forEach(cards => {
-    cards.addEventListener('click', () => {
-        console.log(cards.innerText);
-        navigator.clipboard.writeText(cards.innerText);
-    });
-});
+  function copyCardText() {
+    navigator.clipboard.writeText(this.innerText);
+    console.log(this.innerText);
+  }
+
+  cardText.forEach(cardText => {
+    cardText.onclick = copyCardText;
+  });
 
   return (
     <>
       {card.map((card) => (
         <div key={uuidv4()} className="cards" >
-          <div className="bgGardient" style={{backgroundImage: `linear-gradient(90deg, ${card.from}, ${card.to})`}}>
+          <div className="bgGardient" style={{ backgroundImage: `linear-gradient(90deg, ${card.from}, ${card.to})` }}>
           </div>
           background-image: linear-gradient(90deg, {card.from}, {card.to})
           <div className="describ">
